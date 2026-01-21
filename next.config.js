142449ebd1edcda0c59e844b80e9a92c7bdc7b86
gitcarta <116491616+gitcarta@users.noreply.github.com>
2026-01-21 08:55:02 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export', // Darouri bach i-khdem static
+  images: {
+    unoptimized: true, // Bach l-tsawer i-banou
+  },
 }
-
 module.exports = nextConfig
